b98cb2656a53804d8ce581296f1fa23e6317c2e6
ParaN3xus <paran3xus007@gmail.com>
2023-07-05 17:39:35 +0800

    comment

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function handleRequest(req, res) {
         "port": parsedUrl.port == "" ? (parsedUrl.protocol == "http:" ? "80" : "443") : parsedUrl.port,
         "path": req.url,
         "headers": {
-            "Authorization": req.headers.authorization,
+            "Authorization": req.headers.authorization, // fuck you lower case
             "Content-Type": req.headers["content-type"]
         }
     }
